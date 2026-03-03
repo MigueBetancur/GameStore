f088a50dcb494c88a728eacf044ee5903d50e154
MigueBetancur <miguela.betancurr@uqvirtual.edu.co>
2026-03-02 19:16:34 -0500

    Primer commit

---
+++ src/main/java/co/edu/uniquindio/tallergamestore/TallerGameStoreApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.tallergamestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallerGameStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TallerGameStoreApplication.class, args);
+	}
+
+}
