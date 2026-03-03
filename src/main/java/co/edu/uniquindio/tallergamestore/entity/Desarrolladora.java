ac7f0b8af0515ac1d7f0252c98d63aff9af43956
MigueBetancur <miguela.betancurr@uqvirtual.edu.co>
2026-03-03 01:26:58 -0500

    fix: Se arregló la clase Desarrolladora y se definió nuevamente la base de datos correctamente

--- src/main/java/co/edu/uniquindio/tallergamestore/entity/Desarrolladora.java
+++ src/main/java/co/edu/uniquindio/tallergamestore/entity/Desarrolladora.java
@@ -3,13 +3,15 @@ package co.edu.uniquindio.tallergamestore.entity;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
 
+@Getter
+@Setter
 @Entity
 public class Desarrolladora {
     // Getters y Setters
-    @Getter
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
